d3138fb1a393afb430839f976b771762752c0a5c
Liam M. Perez Lupia <liamperezlupia53@gmail.com>
2023-09-25 03:43:47 -0300

    Improved buttons and styles

--- src/components/ClientCard.tsx
+++ src/components/ClientCard.tsx
@@ -1,6 +1,7 @@
-import {View, Text, StyleSheet, Button} from 'react-native'
+import {View, Text, StyleSheet, Button, Image} from 'react-native'
 import { store } from '../store/store'
 import { TouchableOpacity } from 'react-native-gesture-handler'
+import trash_logo from '../images/basura.png'
 
 interface ClientProps {
     name: string,
@@ -30,7 +31,9 @@ export default function ClientCard ({name, lastname, dni, index, handleUsersView
                 <Text  style={styles.text}>{lastname} </Text>
             </TouchableOpacity>
                 <Text  style={styles.dni}>{dni}</Text>
-            <Button title='X' onPress={() => {handleButton()}}></Button>
+                <TouchableOpacity onPress={() => {handleButton()}} style={styles.logo_wrapper}>
+                    <Image source={trash_logo} style={styles.logo}></Image>
+                </TouchableOpacity>
         </View>
     )
 }
@@ -43,7 +46,8 @@ const styles = StyleSheet.create({
         marginHorizontal: 10,
         justifyContent: "space-between",
         borderRadius: 5,
-        marginVertical: 10
+        marginVertical: 10,
+        alignItems: 'center'
     },
     background: {
         backgroundColor: "#ffda27",
@@ -52,19 +56,29 @@ const styles = StyleSheet.create({
         marginHorizontal: 10,
         justifyContent: "space-between",
         borderRadius: 5,
-        marginVertical: 10
+        marginVertical: 10,
+        alignItems: 'center'
     },
     name: {
         flexDirection: 'row',
     },
     text: {
-        fontSize: 18,
+        fontSize: 20,
         fontWeight: '500',
         color: '#666'
     },
     dni: {
-        fontSize: 18,
+        fontSize: 20,
         fontWeight: '900',
         color: '#333'
+    },
+    logo: {
+        width: 25,
+        height: 25
+    },
+    logo_wrapper: {
+        backgroundColor: "#0a6cb7",
+        padding: 5,
+        borderRadius: 5
     }
 })
--- src/components/ClientScreen.tsx
+++ src/components/ClientScreen.tsx
@@ -1,7 +1,8 @@
-import { ScrollView, Text, StyleSheet, View, Image, Button } from "react-native";
+import { ScrollView, Text, StyleSheet, View, Image, Button, TouchableOpacity } from "react-native";
 import Coppel_logo from "../images/Coppel.png"
 import Section from "./Section";
 import SectionTitle from "./SectionTitle";
+import goBackBtn from '../images/anterior.png'
 
 interface clientProps {
     navigation: any,
@@ -26,26 +27,29 @@ interface clientProps {
     ref2Phone: string
 }
 
-export default function ClientScreen ({navigation, name, lastname, dni, civil, scholarity, city, address, phone, disponability, jobType, jobDescription, jobEarning, ref1Name, ref1Address, ref1Phone, ref2Name, ref2Address, ref2Phone} : clientProps) : JSX.Element {
+export default function ClientScreen ({navigation, name, lastname, dni, birthdate, civil, scholarity, city, address, phone, disponability, jobType, jobDescription, jobEarning, ref1Name, ref1Address, ref1Phone, ref2Name, ref2Address, ref2Phone} : clientProps) : JSX.Element {
 
     return  <ScrollView style={styles.background}>
                 <View style={styles.backgroundHeader}>
                     <Image source={Coppel_logo} style={{ width: 170, height: 70}}/>
-                    <Button title="<" onPress={() => navigation.navigate("Clients")}></Button>
+                    <TouchableOpacity  onPress={() => navigation.navigate("Clients")} style={styles.btn_wrapper}>
+                        <Image source={goBackBtn} style={styles.btn}></Image>
+                    </TouchableOpacity>
                 </View>
                 {/* info personal */}
                 <Section>
                     <>
                     <SectionTitle title="Datos personales"></SectionTitle>
-                    <Text style={styles.text}><Text style={styles.data}>Nombre:</Text> {name}</Text>
-                    <Text style={styles.text}><Text style={styles.data}>Apellido:</Text> {lastname}</Text>
+                    <Text style={styles.text}><Text style={styles.data}>Nombre:</Text> {name ? name : <Text style={styles.blank}>Vacío</Text>}</Text>
+                    <Text style={styles.text}><Text style={styles.data}>Apellido:</Text> {lastname ? lastname :  <Text style={styles.blank}>Vacío</Text>}</Text>
                     <Text style={styles.text}><Text style={styles.data}>DNI:</Text> {dni}</Text>
-                    <Text style={styles.text}><Text style={styles.data}>Estado civil:</Text> {civil}</Text>
-                    <Text style={styles.text}><Text style={styles.data}>Escolaridad:</Text> {scholarity}</Text>
-                    <Text style={styles.text}><Text style={styles.data}>Barrio y localidad:</Text> {city}</Text>
-                    <Text style={styles.text}><Text style={styles.data}>Dirección y numeración:</Text> {address}</Text>
-                    <Text style={styles.text}><Text style={styles.data}>Número de teléfono:</Text> {phone}</Text>
-                    <Text style={styles.text}><Text style={styles.data}>Disponibilidad horaria:</Text> {disponability}</Text>
+                    <Text style={styles.text}><Text style={styles.data}>Fecha de nacimiento:</Text> {birthdate ? birthdate : <Text style={styles.blank}>Vacío</Text>}</Text>
+                    <Text style={styles.text}><Text style={styles.data}>Estado civil:</Text> {civil ? civil : <Text style={styles.blank}>Vacío</Text>}</Text>
+                    <Text style={styles.text}><Text style={styles.data}>Escolaridad:</Text> {scholarity ? scholarity : <Text style={styles.blank}>Vacío</Text>}</Text>
+                    <Text style={styles.text}><Text style={styles.data}>Barrio y localidad:</Text> {city ? city : <Text style={styles.blank}>Vacío</Text>}</Text>
+                    <Text style={styles.text}><Text style={styles.data}>Dirección y numeración:</Text> {address ? address :  <Text style={styles.blank}>Vacío</Text>}</Text>
+                    <Text style={styles.text}><Text style={styles.data}>Número de teléfono:</Text> {phone ? phone : <Text style={styles.blank}>Vacío</Text>}</Text>
+                    <Text style={styles.text}><Text style={styles.data}>Disponibilidad horaria:</Text> {disponability ? disponability : <Text style={styles.blank}>Vacío</Text>}</Text>
                     </>
                 </Section>
 
@@ -53,9 +57,9 @@ export default function ClientScreen ({navigation, name, lastname, dni, civil, s
                 <Section>
                     <>
                     <SectionTitle title="Datos de empleo"></SectionTitle>
-                    <Text style={styles.text}><Text style={styles.data}>Típo de empléo:</Text> {jobType}</Text>
-                    <Text style={styles.text}><Text style={styles.data}>Monto de ingresos:</Text> {jobEarning}</Text>
-                    <Text style={styles.text}><Text style={styles.data}>Descripción de empléo:</Text> {jobDescription}</Text>
+                    <Text style={styles.text}><Text style={styles.data}>Típo de empléo:</Text> {jobType ? jobType : <Text style={styles.blank}>Vacío</Text>}</Text>
+                    <Text style={styles.text}><Text style={styles.data}>Monto de ingresos:</Text> {jobEarning ? jobEarning : <Text style={styles.blank}>Vacío</Text>}</Text>
+                    <Text style={styles.text}><Text style={styles.data}>Descripción de empléo:</Text> {jobDescription ? jobDescription : <Text style={styles.blank}>Vacío</Text>}</Text>
                     </>
                 </Section>
 
@@ -63,17 +67,17 @@ export default function ClientScreen ({navigation, name, lastname, dni, civil, s
                 <Section>
                     <>
                     <SectionTitle title="Datos de referente 1"></SectionTitle>
-                    <Text style={styles.text}><Text style={styles.data}>Nombre:</Text> {ref1Name}</Text>
-                    <Text style={styles.text}><Text style={styles.data}>Dirección:</Text> {ref1Address}</Text>
-                    <Text style={styles.text}><Text style={styles.data}>Número de teléfono:</Text> {ref1Phone}</Text>
+                    <Text style={styles.text}><Text style={styles.data}>Nombre:</Text> {ref1Name ? ref1Name : <Text style={styles.blank}>Vacío</Text>}</Text>
+                    <Text style={styles.text}><Text style={styles.data}>Dirección:</Text> {ref1Address ? ref1Address : <Text style={styles.blank}>Vacío</Text>}</Text>
+                    <Text style={styles.text}><Text style={styles.data}>Número de teléfono:</Text> {ref1Phone ? ref1Phone : <Text style={styles.blank}>Vacío</Text>}</Text>
                     </>
                 </Section>
                 <Section>
                     <>
                     <SectionTitle title="Datos de referente 2"></SectionTitle>
-                    <Text style={styles.text}><Text style={styles.data}>Nombre:</Text> {ref2Name}</Text>
-                    <Text style={styles.text}><Text style={styles.data}>Dirección:</Text> {ref2Address}</Text>
-                    <Text style={styles.text}><Text style={styles.data}>Número de teléfono:</Text> {ref2Phone}</Text>
+                    <Text style={styles.text}><Text style={styles.data}>Nombre:</Text> {ref2Name ? ref2Name : <Text style={styles.blank}>Vacío</Text>}</Text>
+                    <Text style={styles.text}><Text style={styles.data}>Dirección:</Text> {ref2Address ? ref2Address : <Text style={styles.blank}>Vacío</Text>}</Text>
+                    <Text style={styles.text}><Text style={styles.data}>Número de teléfono:</Text> {ref2Phone ? ref2Phone : <Text style={styles.blank}>Vacío</Text>}</Text>
                     </>
                 </Section>
             </ScrollView>
@@ -97,11 +101,24 @@ const styles = StyleSheet.create({
         backgroundColor: "#f9f9d9",
         padding: 6,
         fontWeight: '600',
-        marginVertical: 3
+        marginVertical: 3,
+        borderRadius: 5
     },
     data: {
         backgroundColor: "#0a6cb7",
-        margin: 2,
-        color: '#fff'
+        color: '#fff',
+        
+    },
+    btn: {
+        width: 30,
+        height: 30
+    },
+    btn_wrapper: {
+        backgroundColor: "#0a6cb7",
+        padding: 5,
+        borderRadius: 5
+    },
+    blank: {
+        color: '#c5c5c5'
     }
 })
